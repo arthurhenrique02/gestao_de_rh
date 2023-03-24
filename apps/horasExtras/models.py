f8abfc5ec35881893dadd4ef33cf1534d7fb9d53
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-03-23 23:39:25 -0300

    feito alteração no model ContabilizarHoraExtra

--- apps/horasExtras/models.py
+++ apps/horasExtras/models.py
@@ -6,3 +6,11 @@ class ContabilizarHoraExtra(models.Model):
     # quantidade de hora
     quantidade_horas = models.DecimalField(
         max_digits=5, decimal_places=2, help_text="Quantidade de horas extras")
+
+    # motivo das horas
+    motivo = models.CharField(
+        max_length=250, help_text="Motivo das horas extras")
+
+    # retornar motivo
+    def __str__(self):
+        return self.motivo
