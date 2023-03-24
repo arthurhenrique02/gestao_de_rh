3b4ad5c0d743f947409d9316746953e6f8347583
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-03-23 23:40:42 -0300

    feito alteração no model ContabilizarHoraExtra

--- apps/horasExtras/models.py
+++ apps/horasExtras/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # criar model de horaextra
-class ContabilizarHoraExtra(models.Model):
+class ContabilizarHorasExtra(models.Model):
     # quantidade de hora
     quantidade_horas = models.DecimalField(
         max_digits=5, decimal_places=2, help_text="Quantidade de horas extras")
