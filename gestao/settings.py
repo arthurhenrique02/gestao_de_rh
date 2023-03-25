855db51825c1a915fd6a9533ccf7a6f5675c5ebe
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-03-24 21:31:23 -0300

    adicionado rota para o app 'funcionarios'. Modificado as settings para criação de pacotes para templates e estilos (templates e static, respectivamente)

--- gestao/settings.py
+++ gestao/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "gestao.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates", "static",],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
