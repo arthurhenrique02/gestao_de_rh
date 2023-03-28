66926c45e6ac8382e0bb90719b35a887e1026533
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-03-27 21:25:19 -0300

    alterado index template para mostrar nome do usuario, provisoriamente. E alterado view para retornar os dados coletados do usuario

--- apps/coreApp/views.py
+++ apps/coreApp/views.py
@@ -7,4 +7,11 @@ from django.contrib.auth.decorators import login_required
 # necessario estar logado para acessar a rota
 @login_required(login_url="/accounts/")
 def home(request):  # request como argumento para poder pegar a requisição do usuário
-    return render(request, "index.html")
+
+    # armazenar dados
+    data = {}
+    # pegar o usuario da request e salvar na variavel de armazenamento
+    data["user"] = request.user
+
+    # retornar o template e os dados
+    return render(request, "index.html", data)
