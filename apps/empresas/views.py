7465e315f9a4b275369bb83d26df25e42e77386a
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-03-29 21:26:38 -0300

    criado view para criação de um empresas. Adicionado funcionalidade de 'pareamento' entre a empresa criada e o funcionário que está criando

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -8,3 +8,19 @@ class CriarEmpresa(CreateView):
     # criar view a partir do model empresa
     model = Empresa
     fields = ["nome"]
+
+    # definir metodo form valid
+    # o metodo toma como parametros a propria classe e o form utilizado
+    def form_valid(self, form):
+        # criar objeto para salvar form
+        # será retornado um objeto, nesse caso, o objeto Empresa
+        Empresa_objeto = form.save()
+
+        # pegar objeto funcionario da request,
+        funcionario = self.request.user.funcionario
+
+        # adicionar Empresa ao funcionario
+        funcionario.empresa = Empresa_objeto
+
+        # salvar no Bd a alteração
+        funcionario.save()
