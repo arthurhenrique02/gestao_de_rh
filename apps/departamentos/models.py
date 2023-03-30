64b9826fa4ca94b9b27b22bb44a924a6df9a6717
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-03-29 21:25:13 -0300

    modificado alguns comentários do código

--- apps/departamentos/models.py
+++ apps/departamentos/models.py
@@ -6,6 +6,5 @@ class Departamento(models.Model):
     nome = models.CharField(max_length=200, help_text="Nome do departamento")
 
     # retornar nome
-
     def __str__(self):
         return self.nome
