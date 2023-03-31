aea82bf59917a631da4475a6eb1c403a90ae14c7
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-03-31 18:28:09 -0300

    Criado view para listar funcionarios de uma determinada empresa. Adicionado um Template provisório

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from .views import home
+from .views import home, ListaFuncionarios
 
 
 # incluir view home a url dos funcionarios
 urlpatterns = [
-    # setar como root
-    path("", home),
+    # passar como view e setar na root
+    path("", ListaFuncionarios.as_view(), name='list_funcionarios'),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import ListView
+from .models import Funcionario
+
 
 # definir view home
 # request como argumento para poder pegar a requisição do usuário
 def home(request):
     return HttpResponse("Ola mundo!")
+
+
+# definir classe para listar funcionarios
+class ListaFuncionarios(ListView):
+    # definir model
+    model = Funcionario
+
+    # definir paginação
+    paginate_by = 10
+
+    # alterar queryset para apenas a empresa pertencente ao funcionario logado
+    def get_queryset(self):
+        # pegar a empresa do funcionario logado
+        empresa_logada = self.request.user.funcionario.empresa
+
+        # mudar query
+        return Funcionario.objects.filter(empresa=empresa_logada)
